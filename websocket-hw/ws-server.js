b13e15dd86e115231c422a479f6735b615ae7e32
Danylo <25032002ur@gmail.com>
2026-07-05 17:54:58 +0300

    first commit

---
+++ websocket-hw/ws-server.js
@@ -0,0 +1,24 @@
+const { WebSocketServer } = require("ws");
+
+const wss = new WebSocketServer({ port: 8080 });
+
+console.log("WebSocket-сервер запущено: ws://localhost:8080");
+
+wss.on("connection", (socket) => {
+  console.log("Новий клієнт підключився");
+
+  socket.on("message", (data) => {
+    const text = data.toString();
+    const time = new Date().toLocaleTimeString("uk-UA");
+    const message = `[${time}] ${text}`;
+
+    wss.clients.forEach((client) => {
+      console.log(`Надсилаємо повідомлення клієнту: ${client}`);
+      client.send(message);
+    });
+  });
+
+  socket.on("close", () => {
+    console.log("Клієнт відключився");
+  });
+});
